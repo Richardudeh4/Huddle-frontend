6cfc071416a2037eeb498125ec54ace27e2280bc
Miracle Mark <miraclemark147@gmail.com>
2024-07-03 04:12:47 -0700

    created project

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+
+export default function Home() {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="w-full h-screen flex  justify-center items-center"
+    >
+      <h1>Welcome To Huddle Dashboard</h1>
+      <Button>Hello world</Button>
+    </motion.div>
+  );
+}
